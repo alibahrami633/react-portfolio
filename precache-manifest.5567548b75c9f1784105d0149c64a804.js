9f16bf04fc70aac2db614ad003a20ac8ecabb023
Aliagho <ali.bahrami633@yahoo.com>
2020-07-02 12:47:26 +1000

    Updates

---
+++ precache-manifest.5567548b75c9f1784105d0149c64a804.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3732232f84d816863b39eef315830bf3",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "dd2b03e55b0a7b7493be",
+    "url": "/react-portfolio/static/css/main.569e9306.chunk.css"
+  },
+  {
+    "revision": "3aeb82b61314bb5f0b7f",
+    "url": "/react-portfolio/static/js/2.99b879be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.99b879be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2b03e55b0a7b7493be",
+    "url": "/react-portfolio/static/js/main.68974dfc.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "d56f43dd34305fcee65c594320c04150",
+    "url": "/react-portfolio/static/media/AliBahrami-Resume.d56f43dd.pdf"
+  },
+  {
+    "revision": "1fb3d5844c54f20f79284860178a11f5",
+    "url": "/react-portfolio/static/media/evch-finder.1fb3d584.png"
+  },
+  {
+    "revision": "f17273abdc54033fd509882a3d14b0b0",
+    "url": "/react-portfolio/static/media/randomPasswordGenrator.f17273ab.png"
+  },
+  {
+    "revision": "8107e8dd79fffb0dd70418c744cb371d",
+    "url": "/react-portfolio/static/media/weatherDashboard.8107e8dd.png"
+  },
+  {
+    "revision": "95bea3f77a0fa9891f8b30f6a6e3c6a0",
+    "url": "/react-portfolio/static/media/workdayScheduler.95bea3f7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/alibahrami633/react-portfolio/precache-manifest.b6cc369f9689e24d659f0dc229572dfc.js"
+  "/react-portfolio/precache-manifest.5567548b75c9f1784105d0149c64a804.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alibahrami633/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
