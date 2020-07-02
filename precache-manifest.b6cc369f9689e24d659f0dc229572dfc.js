ea053a385e4699417571cb5f3079dd8eae002391
Aliagho <ali.bahrami633@yahoo.com>
2020-07-02 12:23:09 +1000

    Updates

---
+++ precache-manifest.b6cc369f9689e24d659f0dc229572dfc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8019c07f972a0fbf4971715a01ddb42f",
+    "url": "/alibahrami633/react-portfolio/index.html"
+  },
+  {
+    "revision": "dd2b03e55b0a7b7493be",
+    "url": "/alibahrami633/react-portfolio/static/css/main.569e9306.chunk.css"
+  },
+  {
+    "revision": "3aeb82b61314bb5f0b7f",
+    "url": "/alibahrami633/react-portfolio/static/js/2.99b879be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/alibahrami633/react-portfolio/static/js/2.99b879be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2b03e55b0a7b7493be",
+    "url": "/alibahrami633/react-portfolio/static/js/main.68974dfc.chunk.js"
+  },
+  {
+    "revision": "1b14a7cecc8e1479f0fa",
+    "url": "/alibahrami633/react-portfolio/static/js/runtime-main.0d27a482.js"
+  },
+  {
+    "revision": "d56f43dd34305fcee65c594320c04150",
+    "url": "/alibahrami633/react-portfolio/static/media/AliBahrami-Resume.d56f43dd.pdf"
+  },
+  {
+    "revision": "1fb3d5844c54f20f79284860178a11f5",
+    "url": "/alibahrami633/react-portfolio/static/media/evch-finder.1fb3d584.png"
+  },
+  {
+    "revision": "f17273abdc54033fd509882a3d14b0b0",
+    "url": "/alibahrami633/react-portfolio/static/media/randomPasswordGenrator.f17273ab.png"
+  },
+  {
+    "revision": "8107e8dd79fffb0dd70418c744cb371d",
+    "url": "/alibahrami633/react-portfolio/static/media/weatherDashboard.8107e8dd.png"
+  },
+  {
+    "revision": "95bea3f77a0fa9891f8b30f6a6e3c6a0",
+    "url": "/alibahrami633/react-portfolio/static/media/workdayScheduler.95bea3f7.png"
+  }
+]);
