51f81ae8fc4d83ce964d90e7a6844f6d7fece627
Aliagho <ali.bahrami633@yahoo.com>
2020-07-04 13:22:44 +1000

    solved the routing of the app for github pages using HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Nav from "./components/Nav";
-// import NoMatch from "./pages/NoMatch";
+import NoMatch from "./pages/NoMatch";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper"
 import "./style.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <div className="grad-bg">
         <Nav />
         <Wrapper>
           <Switch>
-            <Route exact path={["/home"]} component={Home} />
+            <Route exact path={["/", "/home"]} component={Home} />
             <Route exact path="/portfolio" component={Portfolio} />
             <Route exact path="/contact" component={Contact} />
-            <Route path="/" component={Home} />
-            {/* <Route component={NoMatch} /> */}
+            <Route component={NoMatch} />
           </Switch>
         </Wrapper>
         <Footer />
